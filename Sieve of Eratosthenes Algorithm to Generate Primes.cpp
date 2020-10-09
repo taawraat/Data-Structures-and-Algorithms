43926a1fc2561aea6be2a769b0de918f7482b3f1
Tawraat <tawraat0187@gmail.com>
2020-10-09 22:46:14 +0600

    Sieve of Eratosthenes Algorithm to Generate Primes

--- Sieve of Eratosthenes Algorithm to Generate Primes.cpp
+++ Sieve of Eratosthenes Algorithm to Generate Primes.cpp
@@ -26,7 +26,7 @@ int main(){
   int N; scanf("%d", &N);
 
   // printing prime numbers 1 to N
-  for(int i = 1; i <= n; i++)
+  for(int i = 1; i <= N; i++)
     if(isPrime(i))
       pf("%d\n", i);
 }
